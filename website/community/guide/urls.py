7360c324f965abdd4e5f42dd8d74f9f564e40b56
khakha93 <jjkha93@gmail.com>
2020-05-18 22:48:28 +0900

    파일 착각해서 다시 붙여넣음

--- website/community/guide/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.guide_main, name='guide_main')
-]
--- website/community/guide/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def guide_main(request):
-    return render(request, 'base.html')
-
-
