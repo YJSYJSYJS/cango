62b35a0fec29a196bf1cd91f0aa7e78b432d0d2f
khakha93 <jjkha93@gmail.com>
2020-05-23 00:42:10 +0900

    main page 수정

--- cango/planner/urls.py
+++ cango/planner/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='main'),
+    path('', views.main, name='main'),
     path('month/', views.view_month, name='month'),
     path('week/', views.view_week, name='week'),
     # path('', views.post_list, name='post_list'),
