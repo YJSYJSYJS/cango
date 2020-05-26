3b554bea0e93662f1b4bd40ff219f5d90947ab94
YounJongSu <dbs106@gmail.com>
2020-05-26 17:45:32 +0900

    view.py
    redirect todo 수정 충돌
    추후 day삭제 바로 해야함

--- cango/planner/views.py
+++ cango/planner/views.py
@@ -42,6 +42,7 @@ def enroll(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('day')
+            return redirect('todo')
     else:
         form = PostForm()
     context = {'form': form}
