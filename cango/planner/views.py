17b5f4856259996c55bf4f7dc6b60c6fbd3a38b4
YounJongSu <dbs106@gmail.com>
2020-05-26 17:47:39 +0900

    views.py
    return redirect('day') 삭제

--- cango/planner/views.py
+++ cango/planner/views.py
@@ -41,7 +41,6 @@ def enroll(request):
             # post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('day')
             return redirect('todo')
     else:
         form = PostForm()
