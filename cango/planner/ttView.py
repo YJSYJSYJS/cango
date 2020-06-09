b1af67ae09fcae9189fa769ff7eeb3e076d67961
khakha93 <jjkha93@gmail.com>
2020-06-09 22:32:56 +0900

    시간표 넣었다....

--- cango/planner/ttView.py
+++ cango/planner/ttView.py
@@ -20,7 +20,26 @@ def tt_view(request):
         days.append(week_str)
 
     # 일정 가져오기
-    posts = Post.objects.all().order_by('end_date')
+    # 필요한거: 제목, 날짜, 시작시간, 종료시간
+    events = []
+    posts = Post.objects.all()
+    for post in posts:
+        e_title = post.title
+        date_str = str(post.start_date).split(" ")
+        d = '-'.join(date_str[0].split('-')[1:])
+        t = date_str[1].split(':')[0]
+        w = datetime.datetime.strptime(date_str[0], "%Y-%m-%d").weekday()
+        events.append(Event(e_title, d, t, w))
 
-    context = {'today': today, 'days': days, 'posts': posts}
+    context = {'today': today, 'days': days, 'events': events}
     return render(request, 'planner/week.html', context)
+
+
+class Event(object):
+    def __init__(self, title, date, time, weekday):
+        self.title = title
+        self.date = date
+        self.time = time
+        self.weekday = (weekday + 1) % 7
+        return
+
