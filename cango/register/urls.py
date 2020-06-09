b5a1195a1b186c62189b0f659e237bd434727fcf
YounJongSu <dbs106@gmail.com>
2020-06-09 15:07:25 +0900

    registry app forms.py genrated
    changed login template by using forms.py

--- cango/register/urls.py
+++ cango/register/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('', RedirectView.as_view(url='/day/')),
     path('register/', views.register),
     path('login/', views.login, name='login'),
-]
+    path('logout/', views.logout, name='logout'),
+    ]
--- cango/register/views.py
+++ cango/register/views.py
@@ -2,30 +2,36 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Puser
-
+from .forms import LoginForm
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'register/login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    # if request.method == 'GET':
+    #     return render(request, 'register/login.html')
+    # elif request.method == 'POST':
+    #     username = request.POST.get('username', None)
+    #     password = request.POST.get('password', None)
 
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            puser = Puser.objects.get(username=username)
-            if check_password(password, puser.password):
-                # 비밀번호가 일치 - 로그인 처리
-                # 세션!
-                # redirect
-                request.session['user'] = puser.id # 세션마다 user키에 puser.id value를 넣어준다. 세션관리는 장고가 알아서 해줌
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호가 틀렸습니다.'
-        
-        return render(request, 'register/login.html', res_data)
+    #     res_data = {}
+    #     if not (username and password):
+    #         res_data['error'] = '모든 값을 입력해야합니다.'
+    #     else:
+    #         puser = Puser.objects.get(username=username)
+    #         if check_password(password, puser.password):
+    #             # 비밀번호가 일치 - 로그인 처리
+    #             # 세션!
+    #             # redirect
+    #             request.session['user'] = puser.id # 세션마다 user키에 puser.id value를 넣어준다. 세션관리는 장고가 알아서 해줌
+    #             return redirect('/')
+    #         else:
+    #             res_data['error'] = '비밀번호가 틀렸습니다.'
+        form = LoginForm()      
+        return render(request, 'register/login.html', {'form': form})
+
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+    
+    return redirect('/')
 
 def register(request):
     if request.method == 'GET':
